46e85013d96dd914844db1a4a7c7d94b0693f28f
Leticia Deodato <leticiamanuelletd@gmail.com>
2021-05-14 16:37:07 -0300

    Retirando os comentários

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -21,7 +21,7 @@
 
         public function add() {
             if ($this->request->is('post')) {
-                $this->request->data['Post']['user_id'] = $this->Auth->user('id'); // Adicionada essa linha
+                $this->request->data['Post']['user_id'] = $this->Auth->user('id');
                 if ($this->Post->save($this->request->data)) {
                     $this->Flash->success('Your post has been saved.');
                     $this->redirect(array('action' => 'index'));
@@ -54,7 +54,6 @@
         public function isAuthorized($user) {
             if (parent::isAuthorized($user)) {
                 if ($this->action === 'add') {
-                    // Todos os usuários registrados podem criar posts
                     return true;
                 }
                 if (in_array($this->action, array('edit', 'delete'))) {
